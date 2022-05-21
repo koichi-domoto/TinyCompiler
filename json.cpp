821bc56fac987ed4bb48b1be5e617cfb43fb4efa
koichi-domoto <3190106004@zju.edu>
2022-05-21 20:51:38 +0800

    correct binaryoperator json

--- json.cpp
+++ json.cpp
@@ -139,51 +139,51 @@ std::string BinaryOperatorExpr::toJSON()
 	std::string opname;
 	switch(Type){
 		case BinaryOperator::SCOPE:
-			opname = "::";
+			opname = "::";break;
 		case BinaryOperator::ARRAY_INDEX:
-			opname = "[]";
+			opname = "[]";break;
 		case BinaryOperator::STRUCT_REF:
-			opname = ".";
+			opname = ".";break;
 		case BinaryOperator::STRUCT_DEREF:
-			opname = "&gt";
+			opname = "&gt";break;
 		case BinaryOperator::AS:
-			opname = "as";
+			opname = "as";break;
 		case BinaryOperator::ADD:
-			opname = "+";
+			opname = "+";break;
 		case BinaryOperator::SUB:
-			opname = "-";
+			opname = "-";break;
 		case BinaryOperator::MUL:
-			opname = "*";
+			opname = "*";break;
 		case BinaryOperator::DIV:
-			opname = "/";
+			opname = "/";break;
 		case BinaryOperator::MOD:
-			opname = "%";
+			opname = "%";break;
 		case BinaryOperator::LEFT_SHIFT:
-			opname = "&lt&lt";
+			opname = "&lt&lt";break;
 		case BinaryOperator::RIGHT_SHIFT:
-			opname = "&gt&gt";
+			opname = "&gt&gt";break;
 		case BinaryOperator::LT:
-			opname = "&lt";
+			opname = "&lt";break;
 		case BinaryOperator::ELT:
-			opname = "&lt=";
+			opname = "&lt=";break;
 		case BinaryOperator::GT:
-			opname = "&gt";
+			opname = "&gt";break;
 		case BinaryOperator::EGT:
-			opname = "&gt=";
+			opname = "&gt=";break;
 		case BinaryOperator::E:
-			opname = "==";
+			opname = "==";break;
 		case BinaryOperator::NE:
-			opname = "!=";
+			opname = "!=";break;
 		case BinaryOperator::AND:
-			opname = "&amp";
+			opname = "&amp";break;
 		case BinaryOperator::XOR:
-			opname = "^";
+			opname = "^";break;
 		case BinaryOperator::OR:
-			opname = "|";
+			opname = "|";break;
 		case BinaryOperator::LOGICAL_AND:
-			opname = "&amp&amp";
+			opname = "&amp&amp";break;
 		case BinaryOperator::LOGICAL_OR:
-			opname = "||";
+			opname = "||";break;
 	};
 	children.push_back(LHS->toJSON());
 	children.push_back(to_json("op",to_json(opname)));
